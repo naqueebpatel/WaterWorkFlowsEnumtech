559ee71a68e353d3ced569a0bb781efb8ac7fd45
FaizanKhan78 <faizankhan9309@gmail.com>
2023-11-09 10:10:02 +0530

    Carsousel

---
+++ waterworksenumtech/src/components/SlidebarData.jsx
@@ -0,0 +1,36 @@
+
+import * as AiIcons from 'react-icons/ai';
+import { MdDashboard, MdDeleteForever, MdUpdate } from 'react-icons/md';
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'DashBoard',
+        path: '/dashboard',
+        icon: <MdDashboard />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Insert',
+        path: '/insert',
+        icon: <AiIcons.AiFillPlusCircle />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Delete',
+        path: '/delete',
+        icon: <MdDeleteForever />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Update',
+        path: '/update',
+        icon: <MdUpdate />,
+        cName: 'nav-text'
+    },
+];
