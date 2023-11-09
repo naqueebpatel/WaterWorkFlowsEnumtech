adada4d2294b905ec23b0c033c34fe5665215f80
farhanmaniyar01 <farhanmaniyaar01@gmail.com>
2023-11-09 14:33:38 +0530

    about us page

--- waterworksenumtech/src/pages/Home.jsx
+++ waterworksenumtech/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import AboutUs from './AboutUs';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+        <AboutUs/>
+        </>
     );
 };
 
