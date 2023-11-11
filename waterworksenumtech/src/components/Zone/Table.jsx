c833f20dedb50eb15bea03e122dd0e0c95554b32
Umershaikh0804 <umernshaikh08@gmail.com>
2023-11-11 13:48:12 +0530

    Dashboard Page

--- waterworksenumtech/src/components/Zone/Table.jsx
+++ waterworksenumtech/src/components/Zone/Table.jsx
@@ -1,53 +1,53 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { LuRefreshCcw } from 'react-icons/lu';
-import '../../styles/zone.css';
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import { LuRefreshCcw } from 'react-icons/lu';
+// import '../../styles/zone.css';
 
-const Table = () => {
-  const [ data, setData ] = useState([]);
+// const Table = () => {
+//   const [ data, setData ] = useState([]);
 
-  const getData = async () => {
-    try {
-      let res = await axios.get("http://localhost:8090/waterwork/get/getZone");
-      setData([ ...res.data ]);
-      console.log(res);
-    } catch (error) {
-      alert("Error");
-    }
-  };
+//   const getData = async () => {
+//     try {
+//       let res = await axios.get("http://localhost:8090/waterwork/get/getZone");
+//       setData([ ...res.data ]);
+//       console.log(res);
+//     } catch (error) {
+//       alert("Error");
+//     }
+//   };
 
-  useEffect(() => {
-    getData();
-  }, []);
+//   useEffect(() => {
+//     getData();
+//   }, []);
 
-  const handleRefresh = () => {
-    getData();
-  };
+//   const handleRefresh = () => {
+//     getData();
+//   };
 
 
 
-  return (
-    <>
-      <table className="zone-table">
-        <thead>
-          <tr>
-            <th scope="col">Zone No</th>
-            <th scope="col">Zone Name</th>
-            <span><LuRefreshCcw /></span>
-          </tr>
-        </thead>
-        <tbody>
-          {data &&
-            data.map((item) => (
-              <tr key={item}>
-                <td >1</td>
-                <td >Faizan</td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <table className="zone-table">
+//         <thead>
+//           <tr>
+//             <th scope="col">Zone No</th>
+//             <th scope="col">Zone Name</th>
+//             <span><LuRefreshCcw /></span>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {data &&
+//             data.map((item) => (
+//               <tr key={item}>
+//                 <td >1</td>
+//                 <td >Faizan</td>
+//               </tr>
+//             ))}
+//         </tbody>
+//       </table>
+//     </>
+//   );
+// };
 
-export default Table;
+// export default Table;
